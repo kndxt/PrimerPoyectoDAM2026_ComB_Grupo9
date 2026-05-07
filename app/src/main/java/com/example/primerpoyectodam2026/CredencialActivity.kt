99d1d8b0baafd0f3bb575667fd565aa97103406e
Marcos028 <machito.draven@gmail.com>
2026-05-07 20:30:59 -0300

    pantallas Agregadas : Pago, MenuAbonar, Disciplinas y Credencial, Iconos

---
+++ app/src/main/java/com/example/primerpoyectodam2026/CredencialActivity.kt
@@ -0,0 +1,14 @@
+package com.example.primerpoyectodam2026
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+
+class CredencialActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_credencial)
+    }
+
+}
---
+++ app/src/main/java/com/example/primerpoyectodam2026/MenuAbonarActivity.kt
@@ -0,0 +1,11 @@
+package com.example.primerpoyectodam2026
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MenuAbonarActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_menu_abonar)
+    }
+}
